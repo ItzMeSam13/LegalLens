1c4f3b6f801cb16f7fcc9a9627d802f8e2e000ad
ItzMeSam <sampreetkalkundrikar8@gmail.com>
2025-07-11 12:09:41 +0530

    server setup

---
+++ server/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 8000
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Welcome to API");
+});
+
+app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}: http://localhost:${PORT}`));
